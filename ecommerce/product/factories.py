737d3fe128ccbe54114ffeae961c95c20ce0f8e1
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2020-03-21 00:27:56 -0300

    Add product tests

--- ecommerce/product/factories.py
+++ ecommerce/product/factories.py
@@ -1,1 +1,30 @@
-from faker import factory
+import factory
+
+from ecommerce.product.models import (
+    Product,
+    Book,
+    EBook,
+)
+
+
+class ProductFactory(factory.DjangoModelFactory):
+    price = factory.Iterator([1, 2])
+    name = factory.Faker('pystr')
+
+    class Meta:
+        model = Product
+        abstract = True
+
+
+class BookFactory(factory.DjangoModelFactory):
+    weight = factory.Iterator([1, 2])
+
+    class Meta:
+        model = Book
+
+
+class EBookFactory(factory.DjangoModelFactory):
+    download_link = factory.Faker('pystr')
+
+    class Meta:
+        model = EBook
