e1475f1ed5ee4f74c42477f8b208e1f00a7ad65e
daniel <drsantos20@gmail.com>
2020-06-06 20:08:49 -0300

    Add create generic product

--- ecommerce/product/viewsets/product_viewset.py
+++ ecommerce/product/viewsets/product_viewset.py
@@ -1,17 +1,29 @@
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from ecommerce.product.utils import get_model_by_name, url_params_validation
+from ecommerce.product.utils import (
+    get_model_by_name,
+    url_params_validation,
+    get_generic_serializer,
+)
 
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(ModelViewSet, CreateModelMixin):
 
     def get_queryset(self):
         model_name = url_params_validation(self.kwargs)
         product = get_model_by_name(model_name)
         return product.objects.all()
 
+    def create(self, request, *args, **kwargs):
+        serializer_from_model = get_generic_serializer(self.kwargs)
+        serializer = serializer_from_model(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
     def get_serializer_class(self):
-        model_name = url_params_validation(self.kwargs)
-        product = get_model_by_name(model_name)
-        serializer_class = product.get_serializer()
+        serializer_class = get_generic_serializer(self.kwargs)
         return serializer_class
